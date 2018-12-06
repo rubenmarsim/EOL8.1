97f9834a7212085b2a708cfb2c4d093d6e79c361
Rubén Martínez <ruben.marsim10@gmail.com>
2018-12-06 14:20:52 +0100

    .

--- EOL/EOL/Program.cs
+++ EOL/EOL/Program.cs
@@ -9,48 +9,101 @@ namespace EOL
     class Program
     {
         #region Variables Globales
-        string _NomUsuari = "";
-        const string _cNom_Complert = "Nom Complert";
+        static string _OpcionGeneral = "";
+        static string _NomUsuari = "";
+        const string _cNomComplert = "Nom Complert";
         const string _cEmail = "Correu Electronic";
         const string _cTelf = "Telefon";
         const string _cAdreca = "Adreca";
         const string _cContraseña = "Paraula Clau";
-        string _Nom_Complert = "";
-        string _Email = "";
-        string _Telf = "";
-        string _Adreca = "";
-        string _Contraseña = "";
-        Dictionary<string, string> _dDatos = new Dictionary<string, string>();
-        Dictionary<string, Dictionary<string, string>> _DG = new Dictionary<string, Dictionary<string, string>>();       
+        static string _Nom_Complert = "";
+        static string _Email = "";
+        static string _Telf = "";
+        static string _Adreca = "";
+        static string _Contraseña = "";
+        static bool _SiExisteUsuario = false;
+        static Dictionary<string, string> _dDatos = new Dictionary<string, string>();
+        static Dictionary<string, Dictionary<string, string>> _DG = new Dictionary<string, Dictionary<string, string>>();       
         #endregion
 
         static void Main(string[] args)
         {
-
+            PreguntasGenerales();
+            Console.ReadKey();            
         }
 
         #region Preguntas
-        private void PreguntasGenerales()
+        private static void PreguntasGenerales()
         {
+            Console.WriteLine("Escull opcio A per Afegir, E per Esborrar i M per Modificar");
+            _OpcionGeneral = Console.ReadLine().Trim().ToUpper();
+            Console.WriteLine("Escriu el Nom d'Usuari");
+            _NomUsuari = Console.ReadLine().Trim();
+            if (_DG.ContainsKey(_NomUsuari))
+            {
+                _SiExisteUsuario = true;
+            }
+            else
+            {
+                _SiExisteUsuario = false;
+            }
 
+            if (_OpcionGeneral.Equals("A"))
+            {
+                if (!_SiExisteUsuario)
+                {
+                    PreguntasAñadir();
+                }
+                else
+                {
+                    Console.WriteLine("L'Usuari ja existeix, proba amb un altre");
+                }                
+            }
+            else if (_OpcionGeneral.Equals("E"))
+            {
+                if (_SiExisteUsuario)
+                {
+                    PreguntasBorrar();
+                }
+                else
+                {
+                    Console.WriteLine("L'Usuari no existeix, proba amb un altre");
+                }                
+            }
+            else if (_OpcionGeneral.Equals("M"))
+            {
+                if (_SiExisteUsuario)
+                {
+                    PreguntasModificar();
+                }
+                else
+                {
+                    Console.WriteLine("L'Usuari no existeix, proba amb un altre");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Introdueix una opcio valida");
+            }
         }
-        private void PreguntasAñadir()
+        private static void PreguntasAñadir()
         {
-
+            //Aqui hay que comprobar si el usuario existe
+            Console.WriteLine("");
         }
-        private void PreguntasBorrar()
+        private static void PreguntasBorrar()
         {
-
+            // Aqui hay que comprobar si el usuario existe
         }
-        private void PreguntasModificar()
+        private static void PreguntasModificar()
         {
-
+            // Aqui hay que comprobar si el usuario existe
         }
         #endregion
 
-        private void LLenarDiccionario()
+        private static void LLenarDiccionario()
         {
-            _dDatos.Add(_cNom_Complert, _Nom_Complert);
+            _dDatos.Add(_cNomComplert, _Nom_Complert);
             _dDatos.Add(_cEmail, _Email);
             _dDatos.Add(_cTelf, _Telf);
             _dDatos.Add(_cAdreca, _Adreca);
@@ -60,11 +113,11 @@ namespace EOL
         }
 
         #region Gestion Datos
-        private void GuardarenDisco()
+        private static void GuardarenDisco()
         {
 
         }
-        private void LeerdeDisco()
+        private static void LeerdeDisco()
         {
 
         }
